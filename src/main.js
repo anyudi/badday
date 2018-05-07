d9c13f11ccf74250d404038f4dbdc90a7c0e77f8
sb <ruoshuilizhui@163.com>
2018-05-07 11:08:59 +0800

    https://github.com/anyudi/badday.git

--- src/main.js
+++ src/main.js
@@ -17,8 +17,26 @@ Vue.config.productionTip = false;
 Vue.use(ElementUI);
 Vue.use(VueResource);
 
+//路由守卫
+router.beforeEach((to, from, next) => {
 
+  //to即将进入的目标路由对象，from当前导航正要离开的路由， next  :  下一步执行的函数钩子
 
+  if(to.path === '/login')  {  next()  }  // 如果即将进入登录路由，则直接放行
+
+  else {     //进入的不是登录路由 to.meta.requiresAuth &&
+    console.log(window.sessionStorage.userMessage);
+    if( window.sessionStorage.userMessage!=='accessToken') {
+      next({ path: '/login' })
+    }
+
+    //下一跳路由需要登录验证，并且还未登录，则路由定向到  登录路由
+
+    else { next() }
+  }
+  //如果不需要登录验证，或者已经登录成功，则直接放行
+  next()
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
